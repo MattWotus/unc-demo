3de6746c6853e4be673d86b917e32f19a5ece0eb
Matthew Wotus <mawotus@gmail.com>
2023-08-21 22:55:12 -0400

    Updated footer.js, hero.js and styles.css

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -22,4 +22,39 @@ export default async function decorate(block) {
     decorateIcons(footer);
     block.append(footer);
   }
+
+  document.querySelector("footer > div > div > div > div > div:nth-child(2) > div:nth-child(1) > ul > li:nth-child(1)").addEventListener("click", function() {
+    window.open(
+    "https://www.facebook.com/uncchapelhill/",
+    "_blank"
+    );
+  })
+
+  document.querySelector("footer > div > div > div > div > div:nth-child(2) > div:nth-child(1) > ul > li:nth-child(2)").addEventListener("click", function() {
+      window.open(
+      "https://twitter.com/unc",
+      "_blank"
+    );
+  })
+
+  document.querySelector("footer > div > div > div > div > div:nth-child(2) > div:nth-child(1) > ul > li:nth-child(3)").addEventListener("click", function() {
+      window.open(
+      "https://www.youtube.com/user/UNCChapelHill",
+      "_blank"
+    );
+  })
+
+  document.querySelector("footer > div > div > div > div > div:nth-child(2) > div:nth-child(1) > ul > li:nth-child(4)").addEventListener("click", function() {
+      window.open(
+      "https://www.instagram.com/uncchapelhill/",
+      "_blank"
+    );
+  })
+
+  document.querySelector("footer > div > div > div > div > div:nth-child(2) > div:nth-child(1) > ul > li:nth-child(5)").addEventListener("click", function() {
+      window.open(
+      "https://pinterest.com/uncchapelhill/",
+      "_blank"
+    );
+  })
 }
---
+++ blocks/hero/hero.js
@@ -0,0 +1,34 @@
+document.querySelector("main > div.section.hero-container.columns-container > div.default-content-wrapper > ul > li:nth-child(1)").addEventListener("click", function() {
+    window.open(
+    "https://www.facebook.com/uncchapelhill/",
+    "_blank"
+  );
+})
+
+document.querySelector("main > div.section.hero-container.columns-container > div.default-content-wrapper > ul > li:nth-child(2)").addEventListener("click", function() {
+    window.open(
+    "https://twitter.com/unc",
+    "_blank"
+  );
+})
+
+document.querySelector("main > div.section.hero-container.columns-container > div.default-content-wrapper > ul > li:nth-child(3)").addEventListener("click", function() {
+    window.open(
+    "https://www.youtube.com/user/UNCChapelHill",
+    "_blank"
+  );
+})
+
+document.querySelector("main > div.section.hero-container.columns-container > div.default-content-wrapper > ul > li:nth-child(4)").addEventListener("click", function() {
+    window.open(
+    "https://www.instagram.com/uncchapelhill/",
+    "_blank"
+  );
+})
+
+document.querySelector("main > div.section.hero-container.columns-container > div.default-content-wrapper > ul > li:nth-child(5)").addEventListener("click", function() {
+    window.open(
+    "https://pinterest.com/uncchapelhill/",
+    "_blank"
+  );
+})
